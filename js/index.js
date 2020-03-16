5d53c165541fd5d314092b872a36e2ad8986145f
kthaovd13 <58517232+kthaovd13@users.noreply.github.com>
2020-03-15 20:51:56 -0500

    added second event listener - click

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+
+//body images - mouseover/mouseout
+
 const bodyImg = document.querySelectorAll(".img-content")
 
 bodyImg[0].addEventListener("mouseover", () => {
@@ -20,3 +23,24 @@ bodyImg[0].addEventListener("mouseover", () => {
     bodyImg[1].style.transform = "scale(1)"
     bodyImg[1].style.transition = "all 0.3s"
   })
+
+  
+  //h2 text color change by click
+
+  const header2 = document.querySelectorAll("h2")
+
+  header2[0].addEventListener('click', (event) => {
+    header2[0].style.color="rebeccapurple";
+  });
+
+  header2[1].addEventListener('click', (event) => {
+    header2[1].style.color="green";
+  });
+
+  header2[2].addEventListener('click', (event) => {
+    header2[2].style.color="pink";
+  });
+
+  header2[3].addEventListener('click', (event) => {
+    header2[3].style.color="red";
+  });
