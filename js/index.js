3e1245adc052118030a896c42a9698d184e7fee3
kthaovd13 <58517232+kthaovd13@users.noreply.github.com>
2020-03-15 19:39:49 -0500

    first event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+const bodyImg = document.querySelectorAll(".img-content")
+
+bodyImg[0].addEventListener("mouseover", () => {
+    bodyImg[0].style.transform = "scale(1.2)"
+    bodyImg[0].style.transition = "all 0.3s"
+  })
+
+  bodyImg[0].addEventListener("mouseout", () => {
+    bodyImg[0].style.transform = "scale(1)"
+    bodyImg[0].style.transition = "all 0.3s"
+  })
+
+  bodyImg[1].addEventListener("mouseover", () => {
+    bodyImg[1].style.transform = "scale(1.2)"
+    bodyImg[1].style.transition = "all 0.3s"
+  })
+
+  bodyImg[1].addEventListener("mouseout", () => {
+    bodyImg[1].style.transform = "scale(1)"
+    bodyImg[1].style.transition = "all 0.3s"
+  })
